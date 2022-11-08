e721c35747b6fcde1c943c8b1f3895a4d581c8fe
ItsOyondi <97532392+ItsOyondi@users.noreply.github.com>
2022-11-08 18:55:30 -0500

    Wrote read data and preprocess data functions

--- R/hello.R
+++ R/hello.R
@@ -13,20 +13,39 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+library(dplyr)
+library(stringr)
+library(tidytext)
+library(ggplot2)
 
+read_data <- function(){
+  data = read.csv("amazon.csv")
   
-}
-pack_j <-function(){
-  print("Josephat new changes to R project")  
-
-  print("Akhila added few changes")
+  data <- data.frame(star_rating = data$star_rating, review = data$review_body) #convert to a dataframe
+  return (data)
 }
 
-
-prof <- function() {
-  print("Hello, world!")
-  print("Josephat")
- 
+data_preprocess <- function(){
+  #data preprocessing
+  data(stop_words)
+  
+  data = read_data()
+  
+  tidy_data <- data %>%
+    unnest_tokens(word, review) %>%
+    anti_join(stop_words) %>%  # remove stop words
+    group_by(star_rating) %>%  #group the term frequencies by star ratings
+    count(word, sort = TRUE) #get the word count for each term
+  
+  
+  tidy_data %>%
+    filter(n > 100) %>%
+    mutate(word = reorder(word, n)) %>%
+    ggplot(aes(word, n)) +
+    geom_col() +
+    xlab(NULL) +
+    coord_flip()
 }
+
+data_preprocess()
+
