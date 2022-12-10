711dbb266409cb03d299f037ed94729a407641a9
dikshashrestha <85287086+dikshashrestha@users.noreply.github.com>
2022-12-10 14:52:57 -0500

    updated authors name and reference

--- R/EmoReader.R
+++ R/EmoReader.R
@@ -16,27 +16,17 @@
 #'
 #' @docType package
 #' @name EmoReader
-#' @author Diskha Shrestha, Roshan Shrestha, Josephat Oyondi, Akhila
-#' @references detailing the functions
+#' @author Diksha Shrestha, Roshan Shrestha, Josephat Oyondi, Akhila
+#' @references 
 #' http://rstudio-pubs-static.s3.amazonaws.com/529538_417d7feffb254fbe9fc3e5294623485e.html
-
-#'exploring the sentiment analysis like positive and negative emotions
-
-#'https://medium.com/swlh/exploring-sentiment-analysis-a6b53b026131
-#'converting our data frame into matrix format
-#'https://statisticsglobe.com/convert-data-frame-to-matrix-in-r
-#'normalize our data using the log normalization method and the NMF dimension reduction approach.
-#'https://satijalab.org/seurat/articles/dim_reduction_vignette.html
-#'dimension reduction method called PCA and how to visualize the data after performing PCA
-#'https://rpubs.com/JanpuHou/278584
-#'K means Clustering
-#'https://www.geeksforgeeks.org/k-means-clustering-in-r-programming/
-#'Hierarchical Clustering
-#'https://www.datacamp.com/tutorial/hierarchical-clustering-R
-#'Syuzget package
-#'https://cran.r-project.org/web/packages/syuzhet/vignettes/syuzhet-vignette.html
-#'getting emotions
-#'https://rdrr.io/cran/syuzhet/man/get_nrc_sentiment.html
+#' https://medium.com/swlh/exploring-sentiment-analysis-a6b53b026131
+#' https://statisticsglobe.com/convert-data-frame-to-matrix-in-r
+#' https://satijalab.org/seurat/articles/dim_reduction_vignette.html
+#' https://rpubs.com/JanpuHou/278584
+#' https://www.geeksforgeeks.org/k-means-clustering-in-r-programming/
+#' https://www.datacamp.com/tutorial/hierarchical-clustering-R
+#' https://cran.r-project.org/web/packages/syuzhet/vignettes/syuzhet-vignette.html
+#' https://rdrr.io/cran/syuzhet/man/get_nrc_sentiment.html
 
 
 #' @aliases EmoReader-package
